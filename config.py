8aad4d9863bb7cb1ade2c8ecb06c1bda9c792b30
itachyutiwa <devmail.itachyutiwa@gmail.com>
2023-05-22 10:12:49 +0000

    Uploadind new config file

---
+++ config.py
@@ -0,0 +1,6 @@
+#Définition du dictionnaire de variables d'environnement sécretes
+environ={}
+environ['CLUSTER']='akilicorp'
+environ['USERNAME']='akone'
+environ['DBNAME']='segmentation_client_banque'
+environ['PASSWORD']='cnB9q1zbygg5yw8G'
